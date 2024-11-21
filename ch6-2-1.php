19ee91f68f9c9c2e7c679575a7da50d17cea8306
KEZEIWEI <C111181123@nkust.edu.tw>
2024-11-21 10:04:53 +0800

    copy new template

--- ch6-2-1.php
+++ ch6-2-1.php
@@ -1,18 +1,21 @@
-<?php 
+<?php
 // 函數顯示標題文字
-function pageHeader() {
-   print "<h3>PHP 與 MySQL 網頁設計</h3>";
-   echo "<hr/>";
-} 
+function pageHeader()
+{
+    print "<h3>PHP 與 MySQL 網頁設計</h3>";
+    echo "<hr/>";
+}
 // 函數顯示註腳文字
-function pageFooter() {
-   print "<hr/>(c)Copyright by 柯智煒<br/>";
-} 
+function pageFooter()
+{
+    print "<hr/> 柯智煒 & C111181123 <br/>";
+}
 ?>
 </head>
+
 <body>
-<?php 
-pageHeader();  // 呼叫函數
-print "<p>在PHP程式使用函數建立標題和註腳文字</p>";
-pageFooter();  // 呼叫函數
-?>
+    <?php
+    pageHeader();  // 呼叫函數
+    print "<p>在PHP程式使用函數建立標題和註腳文字</p>";
+    pageFooter();  // 呼叫函數
+    ?>
